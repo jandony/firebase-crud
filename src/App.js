29e281312b5f3b6b1e9aa06d8471ffd51e9c2740
Jeff Andony <jeff.andony@gmail.com>
2020-08-31 22:17:29 -0600

    Finished all CRUD functionality with minor code cleanup.

--- src/App.js
+++ src/App.js
@@ -16,10 +16,14 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 function App() {
-  const [firebaseData, setFirebaseData] = useState([]);
-  //   console.log(firebaseData);
+  // Declare all useState variables and functions
+  const [data, setData] = useState([]); // collects firebase data
+  let [recipeName, setRecipeName] = useState(""); // collects main input form
+  let [recipeNameRow, setRecipeNameRow] = useState(""); // collects "Recipe Name" table row input form
+  let [descriptionRow, setDescriptionRow] = useState(""); // collects "Description" table row input form
+  let [categoryRow, setCategoryRow] = useState(""); // collects "Category" table row input form
 
-  // Access "recipes" collection from Firebase
+  // READ - Access "recipes" collection from Firebase
   useEffect(() => {
     firebase
       .firestore()
@@ -30,30 +34,80 @@ function App() {
           ...doc.data(),
         }));
 
-        setFirebaseData(newData);
+        setData(newData);
       });
   }, []);
 
-  // useState for input form
-  const [recipeName, setRecipeName] = useState(null);
-
-  // Adds a firebase document to the "recipes" collection
+  // CREATE - Adds a firebase document to the "recipes" collection
   const onAdd = (e) => {
     e.preventDefault();
+    if (recipeName.length > 0) {
+      firebase.firestore().collection("recipes").doc().set({
+        name: recipeName,
+        description: "Another description!",
+        category: "lunch",
+      });
+      // Reset input field to blank
+      setRecipeName("");
+    } else {
+      alert("Text field MUST contain at least 1 character!");
+    }
+  };
 
-    firebase.firestore().collection("recipes").add({
-      name: recipeName,
-      description: "Another description!",
-      category: "lunch",
+  // UDPATE 1/2 - Edit button functions
+  const onEdit = (recipe) => {
+    console.log(recipe);
+    setRecipeNameRow(recipe.name); // set the selected "Recipe Name" to the "recipeNameRow" variable
+    setDescriptionRow(recipe.description); // set the selected "Description" to the "descriptionRow" variable
+    setCategoryRow(recipe.category); // set the selected "Category" to the "categoryRow" variable
+
+    document.getElementById(`editBtn-${recipe.id}`).style.display = "none"; // Hide edit button
+    document.getElementById(`saveBtn-${recipe.id}`).style.display = "inherit"; // Show save button
+
+    document.getElementById(`RecipeNameLabel-${recipe.id}`).style.display =
+      "none"; // Hide "Recipe Name" value
+    document.getElementById(`NewRecipeName-${recipe.id}`).style.display =
+      "inherit"; // Show "Recipe Name" text field
+    document.getElementById(`DescriptionLabel-${recipe.id}`).style.display =
+      "none"; // Hide "Description" value
+    document.getElementById(`NewDescription-${recipe.id}`).style.display =
+      "inherit"; // Show "Description" text field
+    document.getElementById(`CategoryLabel-${recipe.id}`).style.display =
+      "none"; // Hide "Category" value
+    document.getElementById(`NewCategory-${recipe.id}`).style.display =
+      "inherit"; // Show "Category" text field
+  };
+
+  // UPDATE 2/2 - Save button functions
+  const onSave = (recipe) => {
+    firebase.firestore().collection("recipes").doc(recipe.id).set({
+      name: recipeNameRow,
+      description: descriptionRow,
+      category: categoryRow,
     });
+
+    document.getElementById(`editBtn-${recipe.id}`).style.display = "inherit"; // Show edit button
+    document.getElementById(`saveBtn-${recipe.id}`).style.display = "none"; // Hide save button
+
+    document.getElementById(`RecipeNameLabel-${recipe.id}`).style.display =
+      "inherit"; // Show "Recipe Name" value
+    document.getElementById(`NewRecipeName-${recipe.id}`).style.display =
+      "none"; // Hide "Recipe Name" text field
+    document.getElementById(`DescriptionLabel-${recipe.id}`).style.display =
+      "inherit"; // Show "Description" value
+    document.getElementById(`NewDescription-${recipe.id}`).style.display =
+      "none"; // Hide "Description" text field
+    document.getElementById(`CategoryLabel-${recipe.id}`).style.display =
+      "inherit"; // Show "Category" value
+    document.getElementById(`NewCategory-${recipe.id}`).style.display = "none"; // Hide "Category" text field
   };
 
-  // Deletes a firebase document fro mthe "recipes" collection
-  const onDelete = () => {
-    console.log(firebase.firestore().collection("recipes").doc("French Toast"));
+  // DELETE - Deletes a firebase document fro mthe "recipes" collection
+  const onDelete = (i) => {
+    firebase.firestore().collection("recipes").doc(`${i}`).delete();
+    console.log(i);
   };
 
-  //   console.log(recipeName);
   return (
     <React.Fragment>
       <h1>Firebase CRUD App</h1>
@@ -75,16 +129,72 @@ function App() {
           <th>Recipe Name</th>
           <th>Description</th>
           <th>Category</th>
+          <th>ID</th>
+          <th>Update</th>
           <th>Delete?</th>
         </tr>
-        {firebaseData.map((recipe) => {
+        {data.map((recipe) => {
           return (
             <tr key={recipe.id} id={recipe.id}>
-              <td>{recipe.name}</td>
-              <td>{recipe.description}</td>
-              <td>{recipe.category}</td>
+              <td id="rowName">
+                <div id={`RecipeNameLabel-${recipe.id}`}>{recipe.name}</div>
+                <input
+                  type="text"
+                  id={`NewRecipeName-${recipe.id}`}
+                  className="newTextFields"
+                  style={{ width: "90%", display: "none" }}
+                  value={recipeNameRow}
+                  onChange={(e) => {
+                    setRecipeNameRow(e.target.value);
+                  }}
+                />
+              </td>
+              <td id="rowDescription">
+                <div id={`DescriptionLabel-${recipe.id}`}>
+                  {recipe.description}
+                </div>
+                <input
+                  type="text"
+                  id={`NewDescription-${recipe.id}`}
+                  className="newTextFields"
+                  style={{ width: "90%", display: "none" }}
+                  value={descriptionRow}
+                  onChange={(e) => {
+                    setDescriptionRow(e.target.value);
+                  }}
+                />
+              </td>
+              <td id="rowCategory">
+                <div id={`CategoryLabel-${recipe.id}`}>{recipe.category}</div>
+                <input
+                  type="text"
+                  id={`NewCategory-${recipe.id}`}
+                  className="newTextFields"
+                  style={{ width: "90%", display: "none" }}
+                  value={categoryRow}
+                  onChange={(e) => {
+                    setCategoryRow(e.target.value);
+                  }}
+                />
+              </td>
+              <td>{recipe.id}</td>
               <td style={{ textAlign: "center" }}>
-                <button onClick={onDelete}>x</button>
+                <button
+                  id={`editBtn-${recipe.id}`}
+                  onClick={() => onEdit(recipe)}
+                >
+                  Edit
+                </button>
+                <button
+                  id={`saveBtn-${recipe.id}`}
+                  onClick={() => onSave(recipe)}
+                  style={{ display: "none" }}
+                >
+                  Save
+                </button>
+              </td>
+              <td style={{ textAlign: "center" }}>
+                <button onClick={() => onDelete(recipe.id)}>x</button>
               </td>
             </tr>
           );
